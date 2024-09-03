b0df5e2ac819cbabc4adb6636645d10f25592311
Arpit Jana <arpitjana2103@gmail.com>
2024-09-03 16:08:37 +0530

    Learning Strings

--- Udemy JavaScript Course/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy JavaScript Course/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,29 +1,33 @@
-'use strict';
+let s = "relevel";
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// r -- 0 -- 6
+// e -- 1 -- 5
+// l -- 2 -- 4
+// e -- 3 -- 3
+// v -- 4 -- 2
+// e -- 5 -- 1
+// l -- 6 -- 0
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+console.log(s.indexOf("e")); // 1
+console.log(s.lastIndexOf("e")); // 5
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+console.log(s.slice(4));
+// First Index = 4
+// Last Index = 7
+console.log(s.slice(1, 4));
+// First Index = 1
+// Last Index = 4
+console.log(s.toLocaleLowerCase());
+console.log(s.toUpperCase());
+
+let s2 = "Arpit  ";
+console.log(s2.trim());
+console.log(s2.replace("A", "a"));
+console.log(s2.includes("A"));
+console.log(s2.includes("a"));
+console.log(s2.startsWith("A"));
+console.log(s2.endsWith("l"));
+console.log(s2.split("i"));
+console.log(s2.padStart(10, "-"));
+console.log(s2.trim().padEnd(15, "-"));
+console.log(s2.repeat(2));
