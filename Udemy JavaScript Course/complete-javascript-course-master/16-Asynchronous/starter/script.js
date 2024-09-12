8b286bb3e4ccf592d71f4ce37be9623bb36cf1b3
Arpit Jana <arpitjana2103@gmail.com>
2024-09-12 17:43:49 +0530

    AJAX Call

--- Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -3,8 +3,9 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
-
+///////////////////////////////////////////////////////////////////////////
+// PART 01 ////////////////////////////////////////////////////////////////
+/*
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
     request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
@@ -38,3 +39,38 @@ const getCountryData = function (country) {
 
 getCountryData("india");
 getCountryData("portugal");
+*/
+
+///////////////////////////////////////////////////////////////////////////
+// PART 02 ////////////////////////////////////////////////////////////////
+
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+        const [data] = JSON.parse(this.responseText);
+        const flagPng = data.flags.png;
+        const countryName = data.name.common;
+        const region = data.region;
+        const population = (+data.population / 1_000_000).toFixed(1);
+        const languages = Object.values(data.languages).slice(0, 2).join(", ");
+        const currency = Object.values(data.currencies)[0].name;
+
+        const html = `
+        <article class="country">
+          <img class="country__img" src="${flagPng}" />
+          <div class="country__data">
+            <h3 class="country__name">${countryName}</h3>
+            <h4 class="country__region">${region}</h4>
+            <p class="country__row"><span>👫</span>${population} M people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currency}</p>
+          </div>
+        </article>`;
+
+        countriesContainer.insertAdjacentHTML("beforeend", html);
+        countriesContainer.style.opacity = 1;
+    });
+};
