538f87e5c1357e1f39dd0e0373076074101540ca
Arpit Jana <arpitjana2103@gmail.com>
2024-09-12 20:20:20 +0530

    Intro to callback hell

--- Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////////////////////////////////////////
-// PART 01 ////////////////////////////////////////////////////////////////
+// PART 01 -- AJAX CALL ///////////////////////////////////////////////////
 /*
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
@@ -42,23 +42,19 @@ getCountryData("portugal");
 */
 
 ///////////////////////////////////////////////////////////////////////////
-// PART 02 ////////////////////////////////////////////////////////////////
+// PART 02 -- CALL BACK HELL //////////////////////////////////////////////
 
-const getCountryData = function (country) {
-    const request = new XMLHttpRequest();
-    request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
+const baseURL = `https://restcountries.com/v3.1`;
 
-    request.addEventListener("load", function () {
-        const [data] = JSON.parse(this.responseText);
-        const flagPng = data.flags.png;
-        const countryName = data.name.common;
-        const region = data.region;
-        const population = (+data.population / 1_000_000).toFixed(1);
-        const languages = Object.values(data.languages).slice(0, 2).join(", ");
-        const currency = Object.values(data.currencies)[0].name;
+const renderCountry = function (data) {
+    const flagPng = data.flags.png;
+    const countryName = data.name.common;
+    const region = data.region;
+    const population = (+data.population / 1_000_000).toFixed(1);
+    const languages = Object.values(data.languages).slice(0, 2).join(", ");
+    const currency = Object.values(data.currencies)[0].name;
 
-        const html = `
+    const html = `
         <article class="country">
           <img class="country__img" src="${flagPng}" />
           <div class="country__data">
@@ -70,7 +66,51 @@ const getCountryData = function (country) {
           </div>
         </article>`;
 
-        countriesContainer.insertAdjacentHTML("beforeend", html);
-        countriesContainer.style.opacity = 1;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbour = function (country) {
+    // AJAX CALL 01
+    const request = new XMLHttpRequest();
+    request.open("GET", `${baseURL}/name/${country}`);
+    request.send();
+
+    request.addEventListener("load", function () {
+        const [data] = JSON.parse(this.responseText);
+
+        // Render Country 01
+        renderCountry(data);
+
+        const neighbour = data.borders?.[0];
+        if (!neighbour) return;
+
+        // AJAX CALL 02
+        const request = new XMLHttpRequest();
+        request.open("GET", `${baseURL}/alpha/${neighbour}`);
+        request.send();
+
+        request.addEventListener("load", function () {
+            const [data] = JSON.parse(this.responseText);
+
+            // Render Country 02
+            renderCountry(data);
+            const neighbour = data.borders?.[0];
+            if (!neighbour) return;
+
+            // AJAX CALL 03
+            const request = new XMLHttpRequest();
+            request.open("GET", `${baseURL}/alpha/${neighbour}`);
+            request.send();
+
+            request.addEventListener("load", function () {
+                const [data] = JSON.parse(this.responseText);
+
+                // Render Country 03
+                renderCountry(data);
+            });
+        });
     });
 };
+
+getCountryDataAndNeighbour("india");
