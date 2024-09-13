4c0fa6f13cee04014caf2375324fd3304cd5eaaf
Arpit Jana <arpitjana2103@gmail.com>
2024-09-13 13:18:38 +0530

    XMLHttp to Fetch

--- Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ Udemy JavaScript Course/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -2,9 +2,34 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+const baseURL = `https://restcountries.com/v3.1`;
+
+const renderCountry = function (data, neighbour) {
+    const flagPng = data.flags.png;
+    const countryName = data.name.common;
+    const region = data.region;
+    const population = (+data.population / 1_000_000).toFixed(1);
+    const languages = Object.values(data.languages).slice(0, 2).join(", ");
+    const currency = Object.values(data.currencies)[0].name;
+
+    const html = `
+        <article class="country ${neighbour ? neighbour : ""}">
+          <img class="country__img" src="${flagPng}" />
+          <div class="country__data">
+            <h3 class="country__name">${countryName}</h3>
+            <h4 class="country__region">${region}</h4>
+            <p class="country__row"><span>👫</span>${population} M people</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currency}</p> 
+          </div>
+        </article>`;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+};
 
 ///////////////////////////////////////////////////////////////////////////
-// PART 01 -- AJAX CALL ///////////////////////////////////////////////////
+// PART 01 -- [ AJAX CALL ] ///////////////////////////////////////////////
 /*
 const getCountryData = function (country) {
     const request = new XMLHttpRequest();
@@ -42,34 +67,9 @@ getCountryData("portugal");
 */
 
 ///////////////////////////////////////////////////////////////////////////
-// PART 02 -- CALL BACK HELL //////////////////////////////////////////////
+// PART 02 -- [ CALL-BACK HELL ] //////////////////////////////////////////
 
-const baseURL = `https://restcountries.com/v3.1`;
-
-const renderCountry = function (data) {
-    const flagPng = data.flags.png;
-    const countryName = data.name.common;
-    const region = data.region;
-    const population = (+data.population / 1_000_000).toFixed(1);
-    const languages = Object.values(data.languages).slice(0, 2).join(", ");
-    const currency = Object.values(data.currencies)[0].name;
-
-    const html = `
-        <article class="country">
-          <img class="country__img" src="${flagPng}" />
-          <div class="country__data">
-            <h3 class="country__name">${countryName}</h3>
-            <h4 class="country__region">${region}</h4>
-            <p class="country__row"><span>👫</span>${population} M people</p>
-            <p class="country__row"><span>🗣️</span>${languages}</p>
-            <p class="country__row"><span>💰</span>${currency}</p>
-          </div>
-        </article>`;
-
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-};
-
+/*
 const getCountryDataAndNeighbour = function (country) {
     // AJAX CALL 01
     const request = new XMLHttpRequest();
@@ -114,3 +114,39 @@ const getCountryDataAndNeighbour = function (country) {
 };
 
 getCountryDataAndNeighbour("india");
+*/
+
+///////////////////////////////////////////////////////////////////////////
+// PART 02 -- [ FETCH & PROMISES ] ////////////////////////////////////////
+
+const getCountryData = function (country) {
+    fetch(`${baseURL}/name/${country}`)
+        .then((response) => response.json())
+        .then((data) => {
+            renderCountry(data[0]);
+            const neighbour = data[0].borders?.[0];
+            return fetch(`${baseURL}/alpha/${neighbour}`);
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            renderCountry(data[0]);
+        });
+};
+
+// With Async-Await
+
+const getCountryData2 = async function (countryName) {
+    let response = await fetch(`${baseURL}/name/${countryName}`);
+    let data = await response.json();
+    let countryData = data[0];
+
+    renderCountry(countryData);
+    const neighbour = countryData.borders?.[0];
+
+    response = await fetch(`${baseURL}/alpha/${neighbour}`);
+    data = await response.json();
+    countryData = data[0];
+    renderCountry(countryData, "neighbour");
+};
+
+getCountryData2("India");
