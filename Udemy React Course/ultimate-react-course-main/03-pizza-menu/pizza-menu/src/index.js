365db29ccfc358f3eede6be7a5fbcf75957fabcb
Arpit Jana <arpitjana2103@gmail.com>
2024-09-17 17:48:52 +0530

    Passing Data at props

--- Udemy React Course/ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
+++ Udemy React Course/ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
@@ -1,49 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import pizzaData from "./data";
+import "./index.css";
 
 const App = function () {
     return (
-        <>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
-        </>
-    );
-};
-
-const Pizza = function () {
-    return (
-        <div>
-            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-            <h2>Pizza Spinaci</h2>
-            <p>Tomato, mozarella, spinach and ricotta cheese</p>
         </div>
     );
 };
 
 const Header = function () {
-    return <h1>Fast React Pizza Co.</h1>;
-};
-const Menu = function () {
     return (
-        <>
-            <h2>Our menu</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-        </>
+        <header className="header">
+            <h1>Fast React Pizza Co.</h1>
+        </header>
     );
 };
+
+const Pizza = function (props) {
+    console.log(props);
+    return (
+        <div className="pizza">
+            <img src={props.photoName} alt={props.name} />
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
+        </div>
+    );
+};
+
+const Menu = function () {
+    return (
+        <main className="menu">
+            <h2>Our menu</h2>
+            <Pizza
+                name="Pizza Spinaci"
+                ingredients="Tomato, mozarella, spinach and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />
+
+            <Pizza
+                name="Pizza Funghi"
+                ingredients="Tomato, mozarella, spinach and ricotta cheese"
+                photoName="pizzas/funghi.jpg"
+                price={12}
+            />
+        </main>
+    );
+};
+
 const Footer = function () {
     const hour = new Date().getHours();
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
 
     return (
-        <footer>{new Date().toLocaleDateString()} We'r Currently Open</footer>
+        <footer className="footer">
+            {new Date().toLocaleDateString()} We'r Currently Open
+        </footer>
     );
 };
 
