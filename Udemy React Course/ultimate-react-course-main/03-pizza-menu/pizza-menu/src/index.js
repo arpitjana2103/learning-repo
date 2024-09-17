fd361ee640674259088fcabf1d29897abd389319
Arpit Jana <arpitjana2103@gmail.com>
2024-09-17 11:41:18 +0530

    Initial Setup for React Pizza

---
+++ Udemy React Course/ultimate-react-course-main/03-pizza-menu/pizza-menu/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = function () {
+    return (
+        <div>
+            <h1>Hello Arpit !</h1>
+            <Pizza />
+        </div>
+    );
+};
+
+const Pizza = function () {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+            <h2>Pizza Spinaci</h2>
+            <p>Tomato, mozarella, spinach and ricotta cheese</p>
+        </div>
+    );
+};
+
+const rootEl = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootEl);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
