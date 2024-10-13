5ef5ee1545cae9affd1d4512e50bfbdf41beb1fd
Arpit Jana <arpitjana2103@gmail.com>
2024-10-13 16:24:16 +0530

    Watched Box removed

--- Udemy React Course/ultimate-react-course-main/React Practice/05-use-popcorn/src/App.jsx
+++ Udemy React Course/ultimate-react-course-main/React Practice/05-use-popcorn/src/App.jsx
@@ -110,28 +110,6 @@ function Box({ children }) {
   );
 }
 
-// function WatchedBox() {
-//   const [watched, setWatched] = useState(tempWatchedData);
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummery watched={watched} />
-//           <WatchedMovieList watched={watched} />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
 function Movie({ movie }) {
   return (
     <li key={movie.imdbID}>
