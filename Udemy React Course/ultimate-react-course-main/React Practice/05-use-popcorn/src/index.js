61f046ac486a7c737b6788d218474c06ce479e7f
Arpit Jana <arpitjana2103@gmail.com>
2024-10-14 11:17:15 +0530

    Done with Star Rating Component

--- Udemy React Course/ultimate-react-course-main/React Practice/05-use-popcorn/src/index.js
+++ Udemy React Course/ultimate-react-course-main/React Practice/05-use-popcorn/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <StarRating
+            maxRating={5}
+            messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        />
+        <StarRating
+            maxRating={5}
+            size={24}
+            color="red"
+            className="test"
+            defaultRating={2}
+        />
+        {/* <App /> */}
+    </React.StrictMode>
 );
