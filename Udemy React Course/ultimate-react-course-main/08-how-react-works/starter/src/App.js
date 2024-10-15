0f92926923ce31ed823da6c83d6ba2b3dc5a053e
Arpit Jana <arpitjana2103@gmail.com>
2024-10-15 20:47:55 +0530

    Added key prop

--- Udemy React Course/ultimate-react-course-main/08-how-react-works/starter/src/App.js
+++ Udemy React Course/ultimate-react-course-main/08-how-react-works/starter/src/App.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 
 const content = [
   {
+    id: 0,
     summary: "React is a library for building UIs",
     details:
       "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    id: 1,
     summary: "State management is like giving state a home",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
+    id: 2,
     summary: "We can think of props as the component API",
     details:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -19,6 +22,7 @@ const content = [
 ];
 
 export default function App() {
+  console.log(<Tabbed content={content} />);
   return (
     <div>
       <Tabbed content={content} />
@@ -39,7 +43,10 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).id}
+        />
       ) : (
         <DifferentContent />
       )}
