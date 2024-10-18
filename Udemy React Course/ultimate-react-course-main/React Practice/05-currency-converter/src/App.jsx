ab3385e1222f53020d39eecddb0d0d914ff02be2
Arpit Jana <arpitjana2103@gmail.com>
2024-10-18 20:27:48 +0530

    App Updated

--- Udemy React Course/ultimate-react-course-main/React Practice/05-currency-converter/src/App.jsx
+++ Udemy React Course/ultimate-react-course-main/React Practice/05-currency-converter/src/App.jsx
@@ -2,23 +2,39 @@
 
 import { useEffect, useState } from "react";
 const URL = "https://api.frankfurter.app/latest";
+
 export default function App() {
     const [from, setFrom] = useState("USD");
     const [to, setTo] = useState("USD");
     const [amount, setAmount] = useState(0);
     const [resAmount, setResAmount] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(
         function () {
+            const controller = new AbortController();
+            const signal = controller.signal;
             async function fetchAmount() {
-                const resp = await fetch(
-                    `${URL}?amount=${amount}&from=${from}&to=${to}`
-                );
-                const data = await resp.json();
-                setResAmount(data.rates[to]);
+                try {
+                    setIsLoading(true);
+                    const resp = await fetch(
+                        `${URL}?amount=${amount}&from=${from}&to=${to}`,
+                        { signal }
+                    );
+                    const data = await resp.json();
+                    setResAmount(data.rates[to]);
+                    setIsLoading(false);
+                } catch (error) {
+                    if (error.name === "AbortError") return;
+                    console.dir(error);
+                }
             }
             if (amount !== 0 && from !== to) fetchAmount();
             else setResAmount(amount);
+
+            return function () {
+                controller.abort();
+            };
         },
         [from, to, amount]
     );
@@ -30,20 +46,32 @@ export default function App() {
                 value={amount ? amount : ""}
                 onChange={(e) => setAmount(Number(e.target.value))}
             />
-            <select value={from} onChange={(e) => setFrom(e.target.value)}>
+            <select
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+                disabled={isLoading}
+            >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
                 <option value="INR">INR</option>
             </select>
-            <select value={to} onChange={(e) => setTo(e.target.value)}>
+            <select
+                value={to}
+                onChange={(e) => setTo(e.target.value)}
+                disabled={isLoading}
+            >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
                 <option value="INR">INR</option>
             </select>
             <p>
-                Result : {resAmount} {to}
+                {resAmount
+                    ? isLoading
+                        ? "calculating..."
+                        : `${amount} ${from} = ${resAmount} ${to}`
+                    : ""}
             </p>
         </div>
     );
