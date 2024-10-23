6e7b24f8d18f4980f0cc10c120953f791f50b424
Arpit Jana <arpitjana2103@gmail.com>
2024-10-23 19:47:21 +0530

    Working with react router

---
+++ Udemy React Course/ultimate-react-course-main/React Projects/worldwise/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="app" element={<AppLayout />} />
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/" element={<Homepage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default App;
