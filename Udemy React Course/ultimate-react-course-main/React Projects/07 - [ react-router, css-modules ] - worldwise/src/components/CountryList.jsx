9b4f563f60778538dcadd73f8e3c7429e691bab4
Arpit Jana <arpitjana2103@gmail.com>
2024-10-24 12:22:52 +0530

    Showing Country List

---
+++ Udemy React Course/ultimate-react-course-main/React Projects/07 - [ react-router, css-modules ] - worldwise/src/components/CountryList.jsx
@@ -0,0 +1,39 @@
+import Message from "./Message";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+
+function CountryList({ cities, isLoading }) {
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (!cities.length)
+        return (
+            <Message message="Add you first city by clicking on a city on the map" />
+        );
+
+    // [{emoji, country}]
+    const countries = Object.values(
+        cities.reduce(function (acc, city) {
+            return {
+                ...acc,
+                [city.country]: {
+                    country: city.country,
+                    emoji: city.emoji,
+                    id: city.id,
+                },
+            };
+        }, {})
+    );
+
+    return (
+        <ul className={styles.countryList}>
+            {countries.map(function (country) {
+                return <CountryItem country={country} key={country.id} />;
+            })}
+        </ul>
+    );
+}
+
+export default CountryList;
