80cdfadd440d128896937ae61668f78be572dbc9
Arpit Jana <arpitjana2103@gmail.com>
2024-10-25 08:33:43 +0530

    Custom context provider implemented

---
+++ Udemy React Course/ultimate-react-course-main/12-atomic-blog/starter/src/PostContext.js
@@ -0,0 +1,53 @@
+import { faker } from "@faker-js/faker";
+import { createContext } from "react";
+import { useState } from "react";
+
+const PostContext = createContext();
+
+function createRandomPost() {
+    return {
+        title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+        body: faker.hacker.phrase(),
+    };
+}
+
+function PostProvider({ children }) {
+    const [posts, setPosts] = useState(() =>
+        Array.from({ length: 30 }, () => createRandomPost())
+    );
+    const [searchQuery, setSearchQuery] = useState("");
+
+    // Derived state. These are the posts that will actually be displayed
+    const searchedPosts =
+        searchQuery.length > 0
+            ? posts.filter((post) =>
+                  `${post.title}`
+                      .toLowerCase()
+                      .includes(searchQuery.toLowerCase())
+              )
+            : posts;
+
+    function handleAddPost(post) {
+        setPosts((posts) => [post, ...posts]);
+    }
+
+    function handleClearPosts() {
+        setPosts([]);
+    }
+
+    return (
+        <PostContext.Provider
+            value={{
+                posts: searchedPosts,
+                onClearPosts: handleClearPosts,
+                onAddPost: handleAddPost,
+                searchQuery: searchQuery,
+                setSearchQuery: setSearchQuery,
+            }}
+        >
+            {children}
+        </PostContext.Provider>
+    );
+}
+
+export { PostProvider, PostContext };
