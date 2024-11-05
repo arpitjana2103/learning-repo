34119dedb1a59a839e1e184fe1c2aa169a9084a1
Arpit Jana <arpitjana2103@gmail.com>
2024-11-05 08:14:25 +0530

    Setting up Tailwind CSS

--- Udemy React Course/ultimate-react-course-main/React Projects/fast-react-pizza/src/ui/Header.jsx
+++ Udemy React Course/ultimate-react-course-main/React Projects/fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
     return (
-        <header>
-            <Link to="/">Fast React Co.</Link>
+        <header className="bg-yellow-500 uppercase">
+            <Link className="tracking-widest" to="/">
+                Fast React Co.
+            </Link>
             <SearchOrder />
-            <p>Arpit</p>
+            <Username />
         </header>
     );
 }
--- Udemy React Course/ultimate-react-course-main/React Projects/fast-react-pizza/src/ui/Home.jsx
+++ Udemy React Course/ultimate-react-course-main/React Projects/fast-react-pizza/src/ui/Home.jsx
@@ -1,11 +1,17 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
     return (
         <div>
-            <h1 className="text-xl text-yellow-500 text-center">
+            <h1 className="mb-4 text-center text-[100px] font-semibold text-stone-700">
                 The best pizza.
                 <br />
-                Straight out of the oven, straight to you.
+                <span className="text-yellow-500">
+                    Straight out of the oven, straight to you.
+                </span>
             </h1>
+
+            <CreateUser />
         </div>
     );
 }
