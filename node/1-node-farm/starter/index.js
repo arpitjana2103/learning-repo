0e7a4be1977c63efb087d36b5dc484cbb3a5ddcf
Arpit Jana <arpitjana2103@gmail.com>
2025-02-22 19:54:17 +0530

    File reading and writting in NodeJS

--- node/1-node-farm/starter/index.js
+++ node/1-node-farm/starter/index.js
@@ -1,3 +1,21 @@
 const fs = require("fs");
 
-fs.readFileSync();
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
+
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File has been written");
+
+// Non-Blocking, asynchronous
+fs.readFile("./txt/start.txt", "utf-8", function (err, data01) {
+    fs.readFile(`./txt/${data01}.txt`, "utf-8", function (err, data02) {
+        fs.readFile(`./txt/${data01}.txt`, "utf-8", function (err, data03) {
+            const data04 = `${data02}${data01}`;
+            fs.writeFile("./txt/final.txt", "utf-8", data04, function (err) {
+                console.log("Your File hasbeen written");
+            });
+        });
+    });
+});
+
+console.log("Will read File");
