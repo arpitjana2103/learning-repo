436bbbeac371275a596a7eb170ea8f1bcfc25656
Arpit Jana <arpitjana2103@gmail.com>
2025-02-23 16:36:42 +0530

    Defined Routes

--- node/1-node-farm/starter/index.js
+++ node/1-node-farm/starter/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
+////////////////// FILE
+/*
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreate on ${Date.now()}`;
 
@@ -19,3 +23,40 @@ fs.readFile("./txt/start.txt", "utf-8", function (err, data01) {
 });
 
 console.log("Will read File");
+*/
+
+////////////////// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer(function (request, response) {
+    const pathName = request.url;
+
+    switch (pathName) {
+        case "/":
+        case "/overview":
+            response.end("This the Overview");
+            break;
+
+        case "/product":
+            response.end("This is the Product Page");
+            break;
+
+        case "/api":
+            response.writeHead(404, {
+                "Content-type": "application/json",
+            });
+            response.end(data);
+            break;
+
+        default:
+            response.writeHead(404, {
+                "Content-type": "text/html",
+            });
+            response.end("<h1>404 Not Found</h1>");
+    }
+});
+
+server.listen(8000, "127.0.0.1", function () {
+    console.log("Listening to requests on port 8000");
+});
